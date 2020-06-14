5779bfffd42ba8504183545ea0a4ad4be400e113
Crystal <crystalwatkins428@gmail.com>
2020-06-13 22:12:07 -0400

    updates

--- script.js
+++ script.js
@@ -20,18 +20,49 @@ $("#currentDay").text(moment().format("MMMM Do YYYY"));
 var container = $(".time-block");
 var timeRow = $("<div>").addClass("row");
 var timeCol = $("<div>").addClass("col-sm-2 hour");
+var scheduleCol = $("<div>").addClass("col-sm-8 past present future");
+var saveCol = $("<div>").addClass("col-sm-2 saveBtn");
+var textarea= $("<form>").addClass("textarea")
+
 timeRow.append(timeCol);
 container.append(timeRow);
-$(".hour").text("9am");
-var scheduleCol = $("<div>").addClass("col-sm-8 past present future");
+$(".hour").text("");
 timeCol.after(scheduleCol);
-$(".past").text("this is where the main text appears");
-var saveCol = $("<div>").addClass(" col-sm-2 saveBtn");
+$(".past").text("");
 scheduleCol.after(saveCol);
-$(".saveBtn").text("this is where the save button goes");
-   
+$(".saveBtn").text("save button");
+scheduleCol.append(textarea);
+
+    var timeList = [
+     "9 AM",
+     "10 AM",
+     "11 AM",
+     "12 PM",
+     "1 PM",
+     "2 PM",
+     "3 PM",
+     "4 PM",
+     "5 PM",
+ ];
+ 
+for (var i = 0; i < timeList.length; i ++) {
+    timeCol.textContent = timeList[i]; 
+    timeCol.append(timeList[i]);
+    }
 
 
+    
+    // $(document).click(function(){
+    //     $("text").append("<input />");
+    //     $("input").text("#scheduleCol")
+        
+    // })
+
+    // var newRow = $('.row');
+    // for (var i = 0; i < 8; i++) {
+    //   newRow.clone().insertAfter(newRow);
+    // //   timeCol.attr("id", "timeDisplay" + [i]);
+    // };
 
 // use this function event as event listener
 // $("#click-me").on("click", function() {
